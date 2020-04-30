51ab4be63f577c91dcb4a499d39a5811322a384e
Victor Kurauchi <victorkurauchi@gmail.com>
2020-04-30 20:22:18 -0300

    Update palindrome creation less than N

--- palindrome.js
+++ palindrome.js
@@ -1,25 +1,36 @@
-function palindrome(str) {
-  if (str.length <= 1) return false;
-  var re = /[\W_]/g;
-  var lowRegStr = str.toLowerCase().replace(re, '');
-  var reverseStr = lowRegStr.split('').reverse().join(''); 
-  return reverseStr === lowRegStr;
-}
-
-function app(N) {
-  let sum = 0;
-  let i = 0;
-  while (i < N) {
-
-    if (palindrome(i.toString())) {
-      sum += i;
-    }
-
-    i++
+function createPalindrome(input, b, isOdd) {
+  let n = input;
+  let palin = input;
+  
+  if (isOdd == 1)
+    n = Math.floor(n / b);
+  
+  while (n > 0) {
+    palin = palin * b + (n % b);
+    n = Math.floor(n / b);
   }
 
-  console.log(sum);
-  return sum;
+  return palin;
 }
 
-app(10000)
+function generatePalindromes(n) {
+  let number = 0;
+  let palindromes = [];
+  
+  for (let j = 0; j < 2; j++) {
+
+    let i = 1;
+    while ((number = createPalindrome(i, 10, j % 2)) < n)
+    {
+      palindromes.push(number);
+      // console.log(number + " ");
+      i++;
+    }
+  }
+
+  const reducer = (acc, val) => acc + val;
+  return palindromes.reduce(reducer, 0);
+}
+
+console.log(generatePalindromes(10000));
+
